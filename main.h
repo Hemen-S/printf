2be685e3526018ea0cb0de09ccb3d50bb7a23865
bisrat-demere <bisratdemere@gmail.com>
2022-04-19 23:32:12 +0300

    check char

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 int _putchar(char p);
 int print_n(const va_list ls);
 int _printf(const char *format, ...);
-int print_char(const va_list a);
+int print_(const va_list a);
 int print_string(const va_list b);
 int print_modulo(const va_list list);
 int num_digits(int n);
